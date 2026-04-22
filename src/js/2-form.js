2b2b73d484576c87a8254901e44fba10b074a406
YN <julia.nikit.89@gmail.com>
2026-04-22 20:56:08 +0200

    finish home work

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+let formData = { email: "", message: "" };
+
+const form = document.querySelector(".feedback-form");
+
+const STORAGE_KEY = "feedback-form-state";
+
+const populateForm = () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    
+    form.elements.email.value = formData.email || "";
+    form.elements.message.value = formData.message || "";
+  }
+};
+
+populateForm();
+
+form.addEventListener("input", (event) => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: "", message: "" };
+  form.reset();
+});
